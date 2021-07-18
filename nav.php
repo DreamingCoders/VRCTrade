ee1afcbe385a43a91c31ce5995dace5babba0bf1
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-07-17 20:42:33 -0400

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,9 @@
+<h2>BTrade</h2>
+<div class="links">
+  <span class="nav-link">Home</span>
+  <span class="nav-link">Trades</span>
+  <span class="nav-link">Values</span>
+  <div class="seperate"></div>
+  
+  <span class="nav-link"><a href="#">Sign in</a></span>
+  <span class="nav-link"><a href="#" onclick="alert('Register is closed for non-admins\n This may change in the near future!');">Register</a></span>
