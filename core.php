53e77785b65b4ee595528ca4400c676ff35d716f
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-07-17 20:38:53 -0400

    Update core.php

--- core.php
+++ core.php
@@ -1,6 +1,12 @@
 <?php
 $core = "true";
 require('html/nav.php');
+
+$pageView = "true";
+  if($pageView == "true"){
+    require('core/pageHandler.php');
+  }
+  display(model(index()));
 ?>
 <h2>BTrade</h2>
 <p>Handling at it's finest</p>
