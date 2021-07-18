968a415d5d258d1169b5a15f1289781ec5016cb9
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-07-17 20:32:35 -0400

    Create api.php

---
+++ api.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+   
+   $('#content').load("https://www.example.net/api/foobar.php");
+   $('#api').load("https://www.example.net/api/foobar.php", { "players[]": [ "online" ] });
+});
+$.getJSON( "https://www.bprewritten.net/api/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+</script>
+</head>
+<body>
+
+<h2>BPR API</h2>
+<hr>
+
+<p>JSON Data-</p>
+<div id="content">Loading Content...</div>
+<div id="api">Loading API...</div>
+
+
+</body>
+</html>
